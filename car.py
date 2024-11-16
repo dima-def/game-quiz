dbad52fbd177ad49fd83162dfd47279d2caa7600
Maxim Kovtun <kovtun.m@triadasite.ru>
2024-11-16 15:37:38 +0300

    добавили функцию

--- car.py
+++ car.py
@@ -26,41 +26,31 @@ car['state number'] = number
 
 print('Окей, итак вы записали  свою машину. Теперь вы можете вывести данные о ней')
 
-num = input('Нажмите 1 если ходите вывести имя владельца или добавить другого, 2 если марку, 3 если цвет, 4 если лошадиные силы, 5 если гос. номер')
+num = input('Нажмите\n1 если ходите вывести имя владельца или добавить другого,\n2 если марку,\n3 если цвет,\n4 если лошадиные силы,\n5 если гос. номер\n')
+
+
+def changes(car, input_text, field, field_text, print_text):
+    answer = input(input_text)
+    if answer == 'да':
+        new_value = input(field_text)
+        car[field] = new_value
+    elif answer == 'нет':
+        print(print_text, car[field]) 
 
 
 if num == '1':
-    name2 = input('Хотите переименовать владельца?')
-    if name2 == 'да':
-        name = input('Введите имя владельца:')
-        car['ower'] = name
-    elif name2 == 'нет':
-        print('Имя владельца:', car['ower'])
+    changes(
+        car,
+        'Хотите переименовать владельца?',
+        'ower',
+        'Введите имя владельца:',
+        'Имя владельца:'
+    )
 elif num == '2':
-    num3 = input('Хотите изменить марку?')
-    if num3 == 'да':
-        brand = input('Введите марку машны:')
-        car['brand'] = brand
-    elif num3 == 'нет':
-        print(car['brand'])
+    changes(car, 'Хотите изменить марку? (да/нет)', 'brand', 'Введите марку машины:', '')
 elif num == '3':
-    num4 = input('Хотите изменить цвет?')
-    if num4 == 'да':
-        color = input('Введите цвет машины:')
-        car['color'] = color
-    elif num4 == 'нет':
-        print(car['color'])
+    changes(car, 'Хотите изменить цвет? (да/нет)', 'color', 'Введите цвет машины:', '')
 elif num == '4':
-    num5 = input('Хотите изменить лошадиные силы?')
-    if num5 == 'да':
-        hp = input('Введите лошадиные силы:')
-        car['hp'] = hp
-    elif num5 == 'нет':
-        print(car['hp'])
+    changes(car, 'Хотите изменить лошадиные силы? (да/нет)', 'hp', 'Введите лошадиные силы:', '')
 elif num == '5':
-    num6 = input('Хотите изменить гос. номер?')
-    if num6 == 'да':
-        number = input('Введите гос. номер:')
-        car['state number'] = number
-    elif num6 == 'нет':
-        print(car['state number'])
+    changes(car, 'Хотите изменить гос. номер? (да/нет)', 'state number', 'Введите гос. номер:', '')
