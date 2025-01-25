8792bf37f84e53ab54fdb1ba8e19e8ca8e4fa33e
dima-def <dima1kovtun@gmail.com>
2025-01-25 17:11:30 +0300

    убрал ребро у мантки

--- coin.py
+++ coin.py
@@ -3,11 +3,9 @@ import random
 print('Привет! Нажми ENTER чтобы бросить монетку!')
 a = input('')
 
-b = random.randint(1,3)
+b = random.randint(1,2)
 
 if b == 1:
     print('Орёл')
 elif b == 2:
     print('Решка')
-elif b == 3:
-    print('Монетка встала на ребро')
